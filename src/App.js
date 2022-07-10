c29688e203206c1aa1eb318808eee24e4fed6376
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2022-07-10 22:20:35 +0530

    add alt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className='App'>
       <div className='header'>
-        <img src='https://s3.india.com/cricket/wp-content/themes/cricscorecard/images/menubg.jpg?20160223'></img>
+        <img
+          alt='header'
+          src='https://s3.india.com/cricket/wp-content/themes/cricscorecard/images/menubg.jpg?20160223'></img>
         <h2>Get the Details of Previous Matches</h2>
       </div>
       <div className='cards-wrapper'>
